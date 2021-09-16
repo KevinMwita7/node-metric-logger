bc6f55993235b96021b621d4937c68f3b21f0199
KevinMwita7 <kevinmwita7@gmail.com>
2021-09-16 22:02:01 +0300

    bind to port dynamically

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(morgan('combined'));
 // of the format { startTime: Milliseconds, metrics: Array }
 let metrics = [];
 
+app.get('/', (req, res) => {
+    res.send('OK');
+})
+
 // Add metric
 app.post("/:metric", async (req, res) => {
     const { value } = req.body;
@@ -34,14 +38,10 @@ app.delete("/:metric", async (req, res) => {
     return res.send("OK");
 });
 
-app.get('/', (req, res) => {
-    res.send('OK');
-})
-
 // Running on port 0 means choose a random port. Useful because
 // jest runs test in paralled and 3000 might already be in use
 // resulting in an error
-const port = process.env.NODE_ENV === "test" ? 0 : 3000;
+const port = process.env.NODE_ENV === "test" ? 0 : process.env.PORT
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Export the server for testing purposes
